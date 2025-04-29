6934443e9089623ac2729a524e337b19bacd6b6e
Yamen <y.ajjour@ai.uni-hannover.de>
2025-04-29 16:11:22 +0200

    - changed docker setup to in-dockerfile

---
+++ scirag/deploy_scirag.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+. ./scirag/.deploy_variables
+echo $BASE_IMAGE
+
+
+build_docker()
+{
+  echo "building docker "
+  docker build -t "$IMAGE" -f scirag/docker/Dockerfile .
+
+}
+
+
+run_docker()
+{
+  echo "runing docker "
+  echo "$FAST_API_PORT"
+  docker run -it --rm --gpus all -v "$(pwd)":/sciassit  -p 80:80 -w /sciassit --name "$CONTAINER" --tty "$IMAGE"
+
+}
+
+run_service()
+{
+  echo "running service"
+  docker exec -it "$CONTAINER" uvicorn api:app --host 0.0.0.0 --port 80
+
+}
+
+
+push_docker()
+{
+  docker login
+  docker push "$IMAGE"
+}
+
+build_docker
+push_docker
+run_docker
+
