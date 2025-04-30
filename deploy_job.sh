653e8d30a02fc25e46f036d82bb45c3c52a3a151
Yamen <y.ajjour@ai.uni-hannover.de>
2025-04-30 21:36:23 +0200

    - changed docker setup to in-dockerfile

---
+++ deploy_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH --job-name=fine-tuning-2
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=20G
+#SBATCH --time=96:00:00
+#SBATCH --output data/setup_rag.out
+#SBATCH --error data/setup_rag.err
+#SBATCH --gpus=1
+module load Miniforge3
+conda activate sciassist
+
+python scirag/setup_rag.py --debug --path-index data/index --path-dataset data/acl-publication-info.74k.parquet
+  --path-model /home/yajjour/pre-trained-models/DeepSeek-R1-Distill-Qwen-1.5B
