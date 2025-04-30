50538dc4fddb1e3e1471eb0b58cd88e32aab5436
Yamen <y.ajjour@ai.uni-hannover.de>
2025-04-30 21:57:11 +0200

    - changed docker setup to in-dockerfile

--- scirag/setup_api.py
+++ scirag/setup_api.py
@@ -1,3 +1,4 @@
+import os.path
 from typing import Union
 from setup_rag import *
 from fastapi import FastAPI
@@ -6,12 +7,16 @@ from fastapi import FastAPI
 
 
 
-
 def init_ragchain():
     global chain
-    path_index = "data/index"
-    path_dataset = "data/acl-publication-info.74k.parquet"
-    path_model = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+    path = os.path.dirname(os.path.realpath(__file__))
+
+    path_index = f"{path}/../data/index"
+    path_dataset = f"{path}/../data/acl-publication-info.74k.parquet"
+    if os.path.exists("/bigwork/nhwpajjy/pre-trained-models"):
+        path_model = "/bigwork/nhwpajjy/pre-trained-models"
+    else:
+        path_model = "/bigwork/nhwpajjy/pre-trained-models/TinyLlama-1.1B-Chat-v1.0"
 
     if not os.path.exists(path_index):
         create_index( path_dataset=path_dataset, path_index=path_index, debug=False)
