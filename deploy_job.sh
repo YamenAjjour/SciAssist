e8fe3472e5e5867df9b20b93ce5b0b1fc1fca5d4
Yamen <y.ajjour@ai.uni-hannover.de>
2025-06-29 22:40:17 +0200

    fixing job

--- deploy_job.sh
+++ deploy_job.sh
@@ -10,4 +10,4 @@
 module load Miniforge3
 conda activate sciassist
 
-python scirag/setup_rag.py --path-index /mnt/home/yajjour/SciAssist/data/index --path-dataset /mnt/home/yajjour/SciAssist/data/acl-publication-info.74k.parquet --path-model /mnt/home/yajjour/pre-trained-models/DeepSeek-R1-Distill-Qwen-1.5B --query argument mining
+python scirag/setup_rag.py --path-index /mnt/home/yajjour/SciAssist/data/index --path-dataset /mnt/home/yajjour/SciAssist/data/acl-publication-info.74k.parquet --path-model /mnt/home/yajjour/pre-trained-models/DeepSeek-R1-Distill-Qwen-1.5B --query "argument mining"
