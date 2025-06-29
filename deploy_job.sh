3545174d0b8c6c4badd917ffedf62890ee57f4a6
Yamen <y.ajjour@ai.uni-hannover.de>
2025-06-29 22:59:04 +0200

    fixing job

--- deploy_job.sh
+++ deploy_job.sh
@@ -10,4 +10,4 @@
 module load Miniforge3
 conda activate sciassist
 
-python scirag/setup_rag.py --path-index /mnt/home/yajjour/SciAssist/data/index --path-dataset /mnt/home/yajjour/SciAssist/data/acl-publication-info.74k.parquet --path-model /mnt/home/yajjour/pre-trained-models/TinyLlama-1.1B-Chat-v1.0 --query "argument mining"
+python scirag/setup_rag.py --path-index /mnt/home/yajjour/SciAssist/data/index --path-dataset /mnt/home/yajjour/SciAssist/data/acl-publication-info.74k.parquet --path-model /mnt/home/yajjour/pre-trained-models/TinyLlama-1.1B-Chat-v1.0 --query "cross domain robustness of argument mining "
