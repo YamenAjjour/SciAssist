5df18aaeab9d8289834e674d539810eac43b3ed6
Yamen <y.ajjour@ai.uni-hannover.de>
2025-07-01 23:07:54 +0200

    - building streamlit api -dockerizing it

---
+++ scirag/setup_simple_chatui.py
@@ -0,0 +1,45 @@
+import os
+import streamlit as st
+from setup_rag import *
+
+def init_ragchain():
+    global chain
+    path = os.path.dirname(os.path.realpath(__file__))
+
+    path_index = f"{path}/../data/index"
+    path_dataset = f"{path}/../data/acl-publication-info.74k.parquet"
+    if os.path.exists("/bigwork/nhwpajjy/pre-trained-models"):
+        path_model = "/bigwork/nhwpajjy/pre-trained-models/TinyLlama-1.1B-Chat-v1.0"
+    elif os.path.exists("/mnt/home/yajjour/pre-trained-models"):
+        path_model ="/mnt/home/yajjour/pre-trained-models/TinyLlama-1.1B-Chat-v1.0"
+    else:
+        path_model = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+
+    if not os.path.exists(path_index):
+        create_index( path_dataset=path_dataset, path_index=path_index, debug=False)
+
+    chain = create_rag_pipeline(path_index, path_model, True)
+
+chain = None
+init_ragchain()
+
+
+st.title("SciAssist")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content" : prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        answer = chain.run({"query": prompt})
+        response = st.write_stream(answer)
+    st.session_state.messages.append({"role" : "assistant", "content": response})
